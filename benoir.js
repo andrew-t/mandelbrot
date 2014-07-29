70b96fefd7b399a2affee29ee3379a2468a5d334
Andrew Taylor <andrew.taylor@webappuk.com>
2014-07-29 16:18:13 +0100

    much more asm
    this didn't boost performance much at all so i'm not using it in the
    main branch.

--- benoir.js
+++ benoir.js
@@ -1,69 +1,71 @@
-function colour(i, cache) {
-	if (i >= 1)
-		return [0, 0, 0, 255];
-	if (cache[i])
-		return cache[i];
-	var phase = Math.floor(i * 28) % 2;
-	i = i * 28 % 1;
-	switch (phase) {
-		case 0:
-			return cache[i] = [
-				Math.pow(i, 0.2) * 255,
-				Math.pow(i, 0.8) * 255,
-				Math.pow(i, 5) * 255,
-				255
-			];
-		case 1:
-			return cache[i] = [
-				(1 - Math.sqrt(i)) * 255,
-				(1 - i) * 255,
-				(1 - i * i) * 255,
-				255
-			];
-	}
-}
-
-function asmodule() {
+function asmodule(stdlib, foreign, heap) {
 	"use asm";
+
+	var H = new stdlib.Int32Array(heap);
+	var width = 0, height = 0, i = 0, cols = 0;
 
 	function iter(x, y, max, r2) {
 		x = +x; y = +y; max = max|0; r2 = +r2;
-		// take a point called z in the complex plane
-		var zx = 0.0;
-		var zy = 0.0;
-		var newzx = 0.0;
-		var newzy = 0.0;
+		var zx = 0.0, zy = 0.0, newzx = 0.0;
 		var i = 0;
-		var zx2 = 0.0;
-		var zy2 = 0.0;
-		zx = x;
-		zy = y;
-		zx2 = +(x * x);
-		zy2 = +(y * y);
-		do {
-			// let z0 be z^2 + z
+		var zx2 = 0.0, zy2 = 0.0;
+		zx2 = +((zx = x) * x);
+		zy2 = +((zy = y) * y);
+		while (1) {
 			newzx = +(+(zx2 - zy2) + x);
-			newzy = +(+(2.0 * +(zx * zy)) + y);
-			// and z1 be z0^2 + z, and so on
+			zy = +(+(2.0 * +(zx * zy)) + y);
 			zx = newzx;
-			zy = newzy;
-			// if the series of zs will always stay
-			// close to z, and never trend away
-			// that point is in the Mandelbrot set
 			if ((max - (i = (i + 1)|0)) & -2147483648)
 				return i|0;
 			if (+(zx2 = +(zx * zx)) + +(zy2 = +(zy * zy)) > +r2)
 				return i|0;
-		} while (1);
+		}
 		return 0;
 	}
 
+	function init(_width, _height, _cols) {
+		_width = _width|0; _height = _height|0; _cols = _cols|0;
+		width = _width;
+		height = _height;
+		cols = _cols;
+	}
+
+	function mandel(xi, yi, step, max, r2) {
+		xi = +xi; yi = +yi; step = +step; max = max|0; r2 = +r2;
+		var y = 0.0, x = 0.0,
+			u = 0, v = 0, n = 0;
+
+		i = ((cols << 4) - 4)|0;
+		y = yi;
+		for (u = 0; (u - height) & -2147483648; u = (u + 1)|0) {
+			x = xi;
+			for (v = 0; (v - width) & -2147483648; v = (v + 1)|0) {
+				n = iter(x, y, max, r2)|0;
+				if ((n|0) >= (max|0)) {
+					H[(i = (i + 4)|0) >> 2] = 0;
+					H[(i = (i + 4)|0) >> 2] = 0;
+					H[(i = (i + 4)|0) >> 2] = 0;
+				} else {
+					n = (((n|0) % (cols|0)) << 4);
+					H[(i = (i + 4)|0) >> 2] = H[n >> 2];
+					H[(i = (i + 4)|0) >> 2] = H[(n = (n + 4)|0) >> 2];
+					H[(i = (i + 4)|0) >> 2] = H[(n = (n + 4)|0) >> 2];
+				}
+				H[(i = (i + 4)|0) >> 2] = 255;
+				x = +(x + step);
+			}
+			y = +(y + step);
+		}
+	}
+
 	return {
-		iter: iter
+		init: init,
+		mandel: mandel
 	};
 }
 
-var asm = asmodule();
+var buffer, arr, asm, bSize;
+var cols = 512, cols4 = cols * 4;
 
 self.addEventListener('message', function(e) {
 
@@ -75,20 +77,36 @@ self.addEventListener('message', function(e) {
 	var maxr = e.data.maxr;
 	var maxcol = e.data.maxcol;
 
-	var y = yi, x, zx, zy, newzx, newzy, i, r2 = maxr * maxr, imi = 0, im = [], cols = [], px;
-	for (var u = 0; u < height; ++u) {
-		x = xi;
-		for (var v = 0; v < width; ++v) {asm
-			px = colour(asm.iter(x, y, maxcol, r2) / maxcol, cols);
-			im.push(px[0]);
-			im.push(px[1]);
-			im.push(px[2]);
-			im.push(px[3]);
-			x += step;
+	if (!asm) {
+		bSize = (height * width + cols) * 4;
+		if (bSize && 0xffff) {
+			bSize = (bSize & ~0xffff) + 0x10000;
 		}
-		y += step;
+		buffer = new ArrayBuffer(bSize * 4);
+		arr = new Int32Array(buffer);
+		for (var i = 0, j = 0, ii; i < cols; ++i) {
+			if (i >= 256) {
+				ii = (i / 256) - 1;
+				arr[j++] = Math.pow(ii, 0.2) * 255;
+				arr[j++] = Math.pow(ii, 0.8) * 255;
+				arr[j++] = Math.pow(ii, 5.0) * 255;
+				arr[j++] = 255;
+			} else {
+				ii = i / 256;
+				arr[j++] = (1 - Math.sqrt(ii)) * 255;
+				arr[j++] = (1 - ii) * 255;
+				arr[j++] = (1 - ii * ii) * 255;
+				arr[j++] = 255;
+			}
+			if (ii < 0) console.log(i + '!');
+			if (ii > 1) console.log(i);
+		}
+		asm = asmodule({ Math: Math, Int32Array: Int32Array }, {}, buffer);
+		asm.init(width, height, cols);
 	}
 
-	self.postMessage(im);
+	asm.mandel(xi, yi, step, maxcol, maxr * maxr);
+
+	self.postMessage(arr.subarray(cols4, bSize));
 
 });
