8920e14d0c798ac99400a2822ceaff05cd30d55b
Andrew Taylor <andrew.taylor@webappuk.com>
2014-07-30 13:20:01 +0100

    removed asm-specific weirdness while keeping asm-influenced architecture

--- benoir.js
+++ benoir.js
@@ -1,60 +1,51 @@
 function asmodule(stdlib, foreign, heap) {
-	"use asm";
 
 	var H = new stdlib.Int32Array(heap);
 	var width = 0, height = 0, i = 0, cols = 0;
 
 	function iter(x, y, max, r2) {
-		x = +x; y = +y; max = max|0; r2 = +r2;
-		var zx = 0.0, zy = 0.0, newzx = 0.0;
+		var zx = x, zy = y, newzx;
 		var i = 0;
-		var zx2 = 0.0, zy2 = 0.0;
-		zx2 = +((zx = x) * x);
-		zy2 = +((zy = y) * y);
-		while (1) {
-			newzx = +(+(zx2 - zy2) + x);
-			zy = +(+(2.0 * +(zx * zy)) + y);
+		var zx2 = x * x, zy2 = y * y;
+		while (true) {
+			newzx = zx2 - zy2 + x;
+			zy = 2 * zx * zy + y;
 			zx = newzx;
-			if ((max - (i = (i + 1)|0)) & -2147483648)
-				return i|0;
-			if (+(zx2 = +(zx * zx)) + +(zy2 = +(zy * zy)) > +r2)
-				return i|0;
+			if ((max - ++i) & -2147483648)
+				return i;
+			if ((zx2 = zx * zx) + (zy2 = zy * zy) > r2)
+				return i;
 		}
-		return 0;
 	}
 
 	function init(_width, _height, _cols) {
-		_width = _width|0; _height = _height|0; _cols = _cols|0;
 		width = _width;
 		height = _height;
 		cols = _cols;
 	}
 
 	function mandel(xi, yi, step, max, r2) {
-		xi = +xi; yi = +yi; step = +step; max = max|0; r2 = +r2;
-		var y = 0.0, x = 0.0,
-			u = 0, v = 0, n = 0;
+		var y = yi, x, v, n;
 
-		i = ((cols << 4) - 4)|0;
+		i = (cols << 2) - 1;
 		y = yi;
-		for (u = 0; (u - height) & -2147483648; u = (u + 1)|0) {
+		for (var u = 0; (u - height) & -2147483648; ++u) {
 			x = xi;
-			for (v = 0; (v - width) & -2147483648; v = (v + 1)|0) {
-				n = iter(x, y, max, r2)|0;
+			for (v = 0; (v - width) & -2147483648; ++v) {
+				n = iter(x, y, max, r2);
 				if ((n|0) >= (max|0)) {
-					H[(i = (i + 4)|0) >> 2] = 0;
-					H[(i = (i + 4)|0) >> 2] = 0;
-					H[(i = (i + 4)|0) >> 2] = 0;
+					H[++i] = 0;
+					H[++i] = 0;
+					H[++i] = 0;
 				} else {
-					n = (((n|0) % (cols|0)) << 4);
-					H[(i = (i + 4)|0) >> 2] = H[n >> 2];
-					H[(i = (i + 4)|0) >> 2] = H[(n = (n + 4)|0) >> 2];
-					H[(i = (i + 4)|0) >> 2] = H[(n = (n + 4)|0) >> 2];
+					H[++i] = H[n = ((n % cols) << 2)];
+					H[++i] = H[++n];
+					H[++i] = H[++n];
 				}
-				H[(i = (i + 4)|0) >> 2] = 255;
-				x = +(x + step);
+				H[++i] = 255;
+				x += step;
 			}
-			y = +(y + step);
+			y += step;
 		}
 	}
 
