94028ccc7eec51372e405e54cd829c0b75fc2e55
Andrew Taylor <mac@andrewt.net>
2014-08-04 22:44:57 +0100

    refactor, optimisation, proper eventing

--- mandel.js
+++ mandel.js
@@ -1,11 +1,73 @@
-var benoir = new Worker('benoir.js'), c, ctx, im, benoirsLastJob;
-
-function init()
-{
+document.addEventListener('DOMContentLoaded', function() {
 	var x, y, step, maxcol = 200, maxr = 5,
 		maxcolmult = 1.5, maxmaxcol = 3000,
-		zoomfactor = 2, zoomdelay = 0.3,
-		width, height; //seconds
+		zoomfactor = 2, zoomdelay = 0.3, //seconds
+		width, height, benoirsLastJob,
+		benoir = new Worker('benoir.js'), c, ctx, im;
+
+	function zoom(zoomlevel, origin) {
+		var transition = 'transform ' + zoomdelay + 's ease-in-out',
+			transform = 'scale(' + zoomlevel + ')';
+		origin = origin.x + 'px ' + origin.y + 'px';
+		$('#can').css({
+			'-webkit-transform-origin': origin,
+			'-moz-transform-origin': origin,
+			'-o-transform-origin': origin,
+			'-ms-transform-origin': origin,
+			'transform-origin': origin,
+			'-webkit-transform': transform,
+			'-moz-transform': transform,
+			'-o-transform': transform,
+			'-ms-transform': transform,
+			'transform-origin': origin,
+			'-webkit-transition': '-webkit-' + transition,
+			'-moz-transition': '-moz-' + transition,
+			'-o-transition': '-o-' + transition,
+			'-ms-transition': '-ms-' + transition,
+			'transition': transition
+		});
+		return Q.delay(zoomdelay * 1000);
+	}
+
+	function startBenoir(job) {
+		benoirsLastJob = job;
+		var q = Q.defer(),
+			handler = function (result) { 
+				q.resolve(result); 
+			};
+		benoir.addEventListener('message', handler);
+		q.promise.then(function() {
+			benoir.removeEventListener('message', handler);
+		});
+		benoir.postMessage(job);
+		calculating = true;
+		document.body.style.cursor = 'default';
+		return q.promise;
+	}
+
+	function stopBenoir(e) {
+		console.log(e);
+		im.data.set(e.data);
+		ctx.putImageData(im, 0, 0);
+		$('#can').css({
+			'-webkit-transition': 'none',
+			'-moz-transition': 'none',
+			'-o-transition': 'none',
+			'-ms-transition': 'none',
+			'transition': 'none',
+			'-webkit-transform': 'none',
+			'-moz-transform': 'none',
+			'-o-transform': 'none',
+			'-ms-transform': 'none',
+			'transform': 'none'
+		});
+		if (step > zoomfactor * 2 * Number.MIN_VALUE) {
+			document.body.style.cursor = 'pointer';
+			calculating = false;
+		}
+		zoomdelay = (new Date() - started) * 0.001;
+		console.log('frame took ' + zoomdelay + 'ms');
+	}
 
 	// initialisation
 	c = document.getElementById('can');
@@ -26,14 +88,13 @@ function init()
 		step: step,
 		maxr: maxr,
 		maxcol: maxcol
-	}).then(domandel);
+	}).then(stopBenoir);
 
 	// click to zoom
 	var calculating = false;
 	$('#can').click(function(e) {
 		if (calculating) return;
 		$('#history ol').append('<li><img src="' + c.toDataURL() + '"></li>');
-		calculating = true;
 		step /= zoomfactor;
 		x += e.pageX * step * (zoomfactor - 1);
 		y += e.pageY * step * (zoomfactor - 1);
@@ -55,10 +116,7 @@ function init()
 			}),
 			zoom(zoomfactor, {x: e.pageX, y: e.pageY}, zoomdelay)
 		]).then(function(results) {
-			domandel(results[0]);
-			calculating = false;
-			zoomdelay = (new Date() - started) * 0.001;
-			console.log('frame took ' + zoomdelay + 'ms');
+			stopBenoir(results[0]);
 		});
 		document.body.style.cursor = 'default';
 	});
@@ -72,71 +130,12 @@ function init()
 			$(this).val(eval($(this).attr('id')));
 	});
 	$('input').change();
-}
-
-function domandel(e) {
-	for (var i = 0; i < im.data.length; ++i)
-		im.data[i] = e.data[i];
-	ctx.putImageData(im, 0, 0);
-	$('#can').css({
-		'-webkit-transition': 'none',
-		'-moz-transition': 'none',
-		'-o-transition': 'none',
-		'-ms-transition': 'none',
-		'transition': 'none',
-		'-webkit-transform': 'none',
-		'-moz-transform': 'none',
-		'-o-transform': 'none',
-		'-ms-transform': 'none',
-		'transform': 'none'
+	$('#recalculate').click(function() {
+		benoirsLastJob.maxcol = maxcol;
+		benoirsLastJob.maxr = maxr;
+		startBenoir(benoirsLastJob).then(stopBenoir);
 	});
-	document.body.style.cursor = 'pointer';
-}
-
-function zoom(zoomlevel, origin, zoomdelay) {
-	var transition = 'transform ' + zoomdelay + 's ease-in-out',
-		transform = 'scale(' + zoomlevel + ')';
-	origin = origin.x + 'px ' + origin.y + 'px';
-	$('#can').css({
-		'-webkit-transform-origin': origin,
-		'-moz-transform-origin': origin,
-		'-o-transform-origin': origin,
-		'-ms-transform-origin': origin,
-		'transform-origin': origin,
-		'-webkit-transform': transform,
-		'-moz-transform': transform,
-		'-o-transform': transform,
-		'-ms-transform': transform,
-		'transform-origin': origin,
-		'-webkit-transition': '-webkit-' + transition,
-		'-moz-transition': '-moz-' + transition,
-		'-o-transition': '-o-' + transition,
-		'-ms-transition': '-ms-' + transition,
-		'transition': transition
-	});
-	var q = Q.delay(zoomdelay * 1000);
-	q.then(function() {
-	});
-	return q;
-}
-
-function recalculate() {
-	benoirsLastJob.maxcol = maxcol;
-	benoirsLastJob.maxr = maxr;
-	return startBenoir(benoirsLastJob).then(domandel);
-}
-function startBenoir(job) {
-	var benoirsDeferred = Q.defer(),
-		handler = function (result) { 
-			benoirsDeferred.resolve(result); 
-		};
-	benoir.addEventListener('message', handler);
-	benoirsDeferred.promise.then(function() {
-		benoir.removeEventListener('message', handler);
-	});
-	benoir.postMessage(job);
-	return benoirsDeferred.promise;
-}
+});
 
 // boring ui stuff
 function activate(panel) {
