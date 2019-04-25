112fb5e3285a6e3b5cf88b0b634539cb72211100
Andrew Taylor <andrew.taylor@flgtechnology.co.uk>
2019-04-25 13:08:53 +0100

    way better shader version

--- mandel.js
+++ mandel.js
@@ -1,92 +1,5 @@
 'use strict';
 
-const mandelSettings = {
-	maxr: 5,
-	maxcol: 700,
-	maxcolmult: 1.5,
-	maxmaxcol: 10000,
-	tileSide: 5,
-	tileSize: 256
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-	const mapDiv = document.getElementById('complex-plane'),
-		layer = L.tileLayer.canvas({
-			'async': true,
-			continuousWorld: true,
-			attribution: '<a href="http://www.andrewt.net">andrewt.net</a>',
-			maxZoom: 42,
-			maxNativeZoom: 40,
-			bounds: L.latLngBounds(
-				L.latLng(-Infinity, -1e30),
-				L.latLng(Infinity, 1e30))
-		});
-
-	layer.drawTile = (canvas, tilePoint, zoom) => {
-		// Not sure why zoom wasn't being passed in...
-		zoom = complexPlane.getZoom();
-		const side = mandelSettings.tileSide / Math.pow(2, zoom),
-			maxcol = mandelSettings.maxcol * Math.pow(mandelSettings.maxcolmult, zoom);
-		if (window.devicePixelRatio != undefined) {
-			canvas.width = canvas.height = mandelSettings.tileSize * window.devicePixelRatio;
-			canvas.style.width = canvas.style.height = mandelSettings.tileSize + 'px';
-		}
-
-		const shader = new GlslCanvas(canvas);
-		shader.load(glsl);
-		shader.setUniform('u_xi', tilePoint.x * side);
-		shader.setUniform('u_yi', tilePoint.y * side);
-		shader.setUniform('u_step', side / canvas.width);
-		shader.setUniform('u_side', side);
-		shader.setUniform('u_width', canvas.width);
-		shader.setUniform('u_height', canvas.height);
-		shader.setUniform('u_maxcol', (maxcol > mandelSettings.maxmaxcol)
-			? mandelSettings.maxmaxcol
-			: maxcol);
-		shader.pause();
-		layer.tileDrawn(canvas);
-	};
-
-	// Figure out the initial state from the URL and some defaults:
-	let center = [0, -0.5],
-		r = 2;
-	if (location.hash)
-		location.hash.split('#').forEach(function(bit) {
-			const split = bit.indexOf('=');
-			if (~split) {
-				const value = bit.substr(split + 1);
-				switch (bit.substr(0, split)) {
-					case 'x': center[1] = value; break;
-					case 'y': center[0] = value; break;
-					case 'r': r = value; break;
-				}
-			}
-		});
-
-	const screenR = mapDiv.offsetWidth > mapDiv.offsetHeight
-			? mapDiv.offsetWidth
-			: mapDiv.offsetHeight,
-		zoom = Math.floor(Math.log2((mandelSettings.tileSide / r) / (mandelSettings.tileSize / screenR))),
-		pxRatio = mandelSettings.tileSize / mandelSettings.tileSide,
-		complexPlane = L.map(mapDiv, {
-			center: center.map(function (x) { return x * pxRatio }),
-			zoom: zoom,
-			worldCopyJump: false,
-			crs: simple()
-		});
-	complexPlane.addLayer(layer);
-
-	complexPlane.on('move', e => {
-		const center = complexPlane.getCenter(),
-			zoom = (1 << complexPlane.getZoom());
-		document.getElementById('permalink').href =
-			'#x=' + (center.lng / pxRatio) + 
-			'#y=' + (center.lat / pxRatio) + 
-			'#r=' + (mandelSettings.tileSide * screenR /
-				(mandelSettings.tileSize * zoom));
-	});
-});
-
 const glsl = `
 #ifdef GL_ES
 precision highp float;
@@ -141,3 +54,141 @@ void main() {
 	}
 }
 `;
+
+
+const mandelSettings = {
+	maxr: 2,
+	maxcol: 700,
+	maxcolmult: 1.5,
+	maxmaxcol: 10000,
+	tileSide: 5,
+	tileSize: 256
+};
+
+const shaders = [];
+for (let i = 0; i < 16; ++i) {
+	const canvas = document.createElement('canvas');
+	canvas.width = mandelSettings.tileSize;
+	canvas.height = mandelSettings.tileSize;
+	const shader = new GlslCanvas(canvas);
+	shader.load(glsl);
+	shader.setUniform('u_width', canvas.width);
+	shader.setUniform('u_height', canvas.height);
+	shader.pause();
+	shaders.push({ shader, canvas });
+}
+
+let rendering = 0, nextShader = 0;
+function getShader() {
+	const shader = shaders[nextShader++];
+	if (nextShader >= shaders.length)
+		nextShader = 0;
+	return shader;
+}
+const renderQueue = [];
+function resumeRender() {
+	if (--rendering < 5) {
+		const next = renderQueue.shift();
+		if (next) {
+			++rendering;
+			next(getShader());
+		}
+	}
+}
+function enqueueRender(fn) {
+	if (rendering < 16) {
+		++rendering;
+		fn(getShader());
+	} else {
+		renderQueue.push(fn);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const mapDiv = document.getElementById('complex-plane'),
+		layer = L.tileLayer.canvas({
+			'async': true,
+			continuousWorld: true,
+			attribution: '<a href="http://www.andrewt.net">andrewt.net</a>',
+			maxZoom: 42,
+			maxNativeZoom: 40,
+			bounds: L.latLngBounds(
+				L.latLng(-Infinity, -1e30),
+				L.latLng(Infinity, 1e30))
+		});
+
+	layer.drawTile = (tileCanvas, tilePoint, zoom) => {
+		// Not sure why zoom wasn't being passed in...
+		zoom = complexPlane.getZoom();
+		const side = mandelSettings.tileSide / Math.pow(2, zoom),
+			maxcol = mandelSettings.maxcol * Math.pow(mandelSettings.maxcolmult, zoom);
+		if (window.devicePixelRatio != undefined) {
+			tileCanvas.width = tileCanvas.height = mandelSettings.tileSize * window.devicePixelRatio;
+			tileCanvas.style.width = tileCanvas.style.height = mandelSettings.tileSize + 'px';
+		}
+
+		enqueueRender(({ shader, canvas }) => {
+			shader.setUniform('u_xi', tilePoint.x * side);
+			shader.setUniform('u_yi', tilePoint.y * side);
+			shader.setUniform('u_step', side / tileCanvas.width);
+			shader.setUniform('u_side', side);
+			shader.setUniform('u_maxcol', (maxcol > mandelSettings.maxmaxcol)
+				? mandelSettings.maxmaxcol
+				: maxcol);
+			shader.on('render', finalise);
+			shader.play();
+			let done = false;
+			function finalise() {
+				if (done) return;
+				done = true;
+				shader.off('render', finalise);
+				shader.pause();
+				setTimeout(() => {
+					tileCanvas.getContext('2d')
+						.drawImage(canvas, 0, 0);
+					layer.tileDrawn(tileCanvas);
+					resumeRender();
+				}, 25);
+			}
+		});
+	};
+
+	// Figure out the initial state from the URL and some defaults:
+	let center = [0, -0.5],
+		r = 2;
+	if (location.hash)
+		location.hash.split('#').forEach(function(bit) {
+			const split = bit.indexOf('=');
+			if (~split) {
+				const value = bit.substr(split + 1);
+				switch (bit.substr(0, split)) {
+					case 'x': center[1] = value; break;
+					case 'y': center[0] = value; break;
+					case 'r': r = value; break;
+				}
+			}
+		});
+
+	const screenR = mapDiv.offsetWidth > mapDiv.offsetHeight
+			? mapDiv.offsetWidth
+			: mapDiv.offsetHeight,
+		zoom = Math.floor(Math.log2((mandelSettings.tileSide / r) / (mandelSettings.tileSize / screenR))),
+		pxRatio = mandelSettings.tileSize / mandelSettings.tileSide,
+		complexPlane = L.map(mapDiv, {
+			center: center.map(function (x) { return x * pxRatio }),
+			zoom: zoom,
+			worldCopyJump: false,
+			crs: simple()
+		});
+	complexPlane.addLayer(layer);
+
+	complexPlane.on('move', e => {
+		const center = complexPlane.getCenter(),
+			zoom = (1 << complexPlane.getZoom());
+		document.getElementById('permalink').href =
+			'#x=' + (center.lng / pxRatio) + 
+			'#y=' + (center.lat / pxRatio) + 
+			'#r=' + (mandelSettings.tileSide * screenR /
+				(mandelSettings.tileSize * zoom));
+	});
+});
